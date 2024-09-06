20a30a528d6dcc185964693f5dfd778936b76b65
Musa-254 <ngarimusa53@gmail.com>
2024-09-06 10:52:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//gretings function
+function greet(name){
+    let greeting="hello"+name;
+     return "hello" +name;
+ };
+
+   console.log(greet("musa"));
+
+ // square of a number
+let square=(number)=>{
+   const result=number** 2;
+   
+console.log("result");}
+square(5);
+//function of an area of a rectangle
+function areaOfRectangle(l,w) {
+return l*w;
+}
+
+console.log(areaOfRectangle(5,3));
+
+
+let areaOfRect=(l,w)=>{
+   const result=l*w;
+   console.log(result);
+}
+
+areaOfRect(5,4)
